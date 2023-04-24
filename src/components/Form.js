331dceadc44bf5d80232553638d480b3cd89f9ac
brianr9799 <brianr9799@gmail.com>
2023-04-24 18:39:31 -0400

    first commit-favlinks project

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,23 +1,44 @@
-import { useState } from 'react'
+//import { useState } from 'react'
 
-const Form = () => {
+const Form = (props) => {
+  
+  
   const handleChange = (event) => {
-    /*
-            TODO - Logic for changing state based on form changes
-        */
+    /* TODO - Logic for changing state based on form changes */
+    //console.log('new value', event.target.value)
+
+    const value = event.target.value
+    
+    props.setFormData({
+      ...props.formData,
+      [event.target.name]: value
+      
+    })
+    
   }
 
   const onFormSubmit = (event) => {
     // to prevent page reload on form submit
     event.preventDefault()
 
-    /*
-            TODO - Logic for calling props to handle submission and setting state changes
-        */
+    /* TODO - Logic for calling props to handle submission and setting state changes */
+    alert('Data was submitted: ' );
+    props.handleSubmit()
+    
   }
 
   return (
-    <form>
+    <form onSubmit = {onFormSubmit}>
+      
+      {/* first input name */}
+      <label> Name: </label>
+      <input type='text' name= 'linkName' value= {props.formData.linkName} onChange={handleChange}/>
+      
+      {/* second input url */}
+      <label> Url: </label>
+      <input type= 'text' name= 'linkUrl' value= {props.formData.linkUrl} onChange={handleChange}/>
+
+      <input type= 'submit' value= 'Submit'/>
       {/* TODO - Logic for returning a form element with labels and inputs for link name and URL */}
     </form>
   )
--- src/components/LinkContainer.js
+++ src/components/LinkContainer.js
@@ -1,30 +1,43 @@
 import { useState } from 'react'
-// import Table from './Table';
-// import Form from './Form';
+import Table from './Table';
+import Form from './Form';
 
 const LinkContainer = (props) => {
+
+
+  const[formData, setFormData] = useState({linkName:'', linkUrl:''})
+  const[linkData, setLinkData] = useState([])
+
   const handleRemove = (index) => {
-    /*
-            TODO - Create logic for setting the state to filter array and remove favLink at index
-        */
+    /* TODO - Create logic for setting the state to filter array and remove favLink at index */
+
+    setLinkData(favlinks => {
+      return favlinks.filter((_,i) => i !== index)
+    })
+    
+    alert("removed");
   }
 
-  const handleSubmit = (favLink) => {
-    /*
-            TODO - Create logic to set state and add new favLink to favLinks array in state
-        */
+  const handleSubmit = () => {
+    /* TODO - Create logic to set state and add new favLink to favLinks array in state */
+    const newFavLinks = {name: formData.linkName,URL: formData.linkUrl }
+    setLinkData([...linkData, newFavLinks])
+
   }
 
   return (
     <div className="container">
       <h1>My Favorite Links</h1>
       <p>Add a new url with a name and link to the table.</p>
-      {/*TODO - Add Table Component */}
-
+      
+      <Table removeLink={handleRemove} linkData={linkData}/>
+      
       <br />
 
       <h3>Add New</h3>
-      {/*TODO - Add Form Component */}
+
+      <Form handleSubmit ={handleSubmit} formData={formData} setFormData={setFormData}/>
+      
     </div>
   )
 }
